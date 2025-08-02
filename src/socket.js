6908275316eb6ef117f4e4844214c65f330b9609
OrangeCatLoves <e1121707@u.nus.edu>
2025-08-02 17:53:01 +0800

    Changed some URL for deployment purposes in socket.js.

--- src/socket.js
+++ src/socket.js
@@ -4,8 +4,10 @@ const { registerDuelHandlers } = require('./duelSocket')
 const { registerSoloHandlers } = require('./soloSocket')
 
 function initSocket(server) {
+  // Read FRONTEND_URL from env, fallback to localhost for dev
+  const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
   const io = new Server(server, {
-    cors: { origin: 'http://localhost:5173', methods: ['GET','POST'] }
+    cors: { origin: FRONTEND_URL.split(','), methods: ['GET','POST'] }
   })
 
   // wire up duel and solo modes
